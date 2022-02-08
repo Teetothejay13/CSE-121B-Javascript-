46768ff5b3dfb0902fbc1321f5086b09a44966f1
TJ <teetothejay13@gmail.com>
2022-02-08 13:38:00 -0700

    added week 7

---
+++ Week 7 (Final Project)/normal-origami.js
@@ -0,0 +1,13 @@
+// With this, I will create and add new items to the normal origami page, as if they had been submitted by
+// others, and I was approving them. They will appear below the projects that are already part of the html
+// file.
+
+// I'll be reading these from their own dedicated json files, so I need to open json files
+
+
+
+// First, we need to create the spot for the items, including an <h2>, a <p>, and a <img>
+// 
+
+
+// element.classList.add()
